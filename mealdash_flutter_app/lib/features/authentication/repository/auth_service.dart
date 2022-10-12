b1b4644bcf39f9b42722a534885539780c4209b1
sduzair <sduzairahmed@gmail.com>
2022-10-12 01:18:45 -0400

    user signup model

--- mealdash_flutter_app/lib/features/authentication/repository/auth_service.dart
+++ mealdash_flutter_app/lib/features/authentication/repository/auth_service.dart
@@ -1,9 +1,13 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:mealdash_app/features/authentication/models/user_login_model.dart';
 import 'package:mealdash_app/features/authentication/models/user_signup_model.dart';
 import 'package:mealdash_app/utils/constants.dart' as constants;
 
 class AuthService {
+  static bool _isLoggedIn = false;
+  static get isLoggedIn => _isLoggedIn;
+
   static Future<http.Response> signUp(UserSignUpModel userSignUpModel) async {
     final response = await http.post(
       Uri.parse('${constants.apiUrl}/signup'),
@@ -12,6 +16,23 @@ class AuthService {
       },
       body: jsonEncode(userSignUpModel.toJson()),
     );
+    if (response.statusCode == 201) {
+      _isLoggedIn = true;
+    }
+    return response;
+  }
+
+  static Future<http.Response> login(UserLoginModel userLoginModel) async {
+    final response = await http.post(
+      Uri.parse('${constants.apiUrl}/login'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(userLoginModel.toJson()),
+    );
+    if (response.statusCode == 200) {
+      _isLoggedIn = true;
+    }
     return response;
   }
 }

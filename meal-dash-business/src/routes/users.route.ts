aac25785fd31e3b72824573be8db62b8bd853a27
azeez-ahmad <azeez.aziiz@gmail.com>
2022-11-11 20:42:36 -0500

    added auth on user endpoints

--- meal-dash-business/src/routes/users.route.ts
+++ meal-dash-business/src/routes/users.route.ts
@@ -3,6 +3,7 @@ import UsersController from '@controllers/users.controller';
 import { Routes } from '@interfaces/routes.interface';
 import validationMiddleware from '@middlewares/validation.middleware';
 import { CreateUserDto } from '@/dtos/createusers.dto';
+import authMiddleware from '@/middlewares/auth.middleware';
 
 // UsersRoute class to handle all the routes for users
 class UsersRoute implements Routes {
@@ -15,11 +16,11 @@ class UsersRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.usersController.getUsers);
-    this.router.get(`${this.path}/:user_id(\\d+)`, this.usersController.getUserById);
-    this.router.post(`${this.path}`, validationMiddleware(CreateUserDto, 'body'), this.usersController.createUser);
-    this.router.put(`${this.path}/:user_id(\\d+)`, validationMiddleware(CreateUserDto, 'body', true), this.usersController.updateUser);
-    this.router.delete(`${this.path}/:user_id(\\d+)`, this.usersController.deleteUser);
+    this.router.get(`${this.path}`, authMiddleware, this.usersController.getUsers);
+    this.router.get(`${this.path}/:user_id(\\d+)`, authMiddleware, this.usersController.getUserById);
+    this.router.post(`${this.path}`, authMiddleware, validationMiddleware(CreateUserDto, 'body'), this.usersController.createUser);
+    this.router.put(`${this.path}/:user_id(\\d+)`, authMiddleware, validationMiddleware(CreateUserDto, 'body', true), this.usersController.updateUser);
+    this.router.delete(`${this.path}/:user_id(\\d+)`, authMiddleware, this.usersController.deleteUser);
   }
 }
 

e9c42b264cad22cd01c820e7fa2a15c65c8c72ff
azeez-ahmad <azeez.aziiz@gmail.com>
2022-11-17 23:08:30 -0500

    auth security code verification update

--- meal-dash-business/src/services/auth.service.ts
+++ meal-dash-business/src/services/auth.service.ts
@@ -14,6 +14,8 @@ import { CreateUserDto } from '@/dtos/createusers.dto';
 import MailService from '@/helper/email.helper';
 import { GOOGLE_API_KEY } from '@config';
 import { createClient } from "@google/maps";
+import { UpdateRadiusDto } from '@/dtos/radius.dto';
+import { VerifyUserDto } from '@/dtos/verifyuser.dto';
 const googleMapsClient = createClient({
   key: GOOGLE_API_KEY,
   Promise: Promise
@@ -29,8 +31,11 @@ class AuthService extends Repository<UserEntity> {
 
     const location = await this.getLatLng(userData);
     const hashedPassword = await hash(userData.user_password, 10);
-    const createUserData: User = await UserEntity.create({ ...userData, user_password: hashedPassword, ...location }).save();
+    let security_code = Math.floor(100000 + Math.random() * 900000);
+    const createUserData: User = await UserEntity.create({ ...userData, user_activation_code: security_code, user_password: hashedPassword, ...location }).save();
     MailService.getInstance().sendMail(createUserData);
+    createUserData.user_password = undefined;
+    createUserData.user_activation_code= undefined;
     return createUserData;
   }
 
@@ -40,6 +45,8 @@ class AuthService extends Repository<UserEntity> {
     const findUser: User = await UserEntity.findOne({ where: { user_email: userData.user_email } });
     if (!findUser) throw new HttpException(409, `This email ${userData.user_email} was not found`);
 
+    if(!findUser.user_status) throw new HttpException(409, `This email ${userData.user_email} is not verified`);
+
     const isPasswordMatching: boolean = await compare(userData.user_password, findUser.user_password);
     if (!isPasswordMatching) throw new HttpException(409, "Password not matching");
 
@@ -47,6 +54,7 @@ class AuthService extends Repository<UserEntity> {
     const cookie = this.createCookie(tokenData);
 
     findUser.user_password = undefined;
+    findUser.user_activation_code= undefined;
     return { cookie, findUser };
   }
 
@@ -57,6 +65,7 @@ class AuthService extends Repository<UserEntity> {
     if (!findUser) throw new HttpException(409, "User doesn't exist");
 
     findUser.user_password = undefined;
+    findUser.user_activation_code= undefined;
     return findUser;
   }
 
@@ -92,6 +101,38 @@ class AuthService extends Repository<UserEntity> {
   }
   public createCookie(tokenData: TokenData): string {
     return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn};`;
+  }
+
+
+   //updateUser update user by id
+   public async updateVenderRadius(updateRadiusDto: UpdateRadiusDto): Promise<User> {
+    if (isEmpty(updateRadiusDto)) throw new HttpException(400, "updateRadiusDto is empty");
+
+    const findUser: User = await UserEntity.findOne({ where: { user_id: updateRadiusDto.user_id, user_login: updateRadiusDto.user_login} });
+    if (!findUser) throw new HttpException(409, "User doesn't exist");
+
+    await UserEntity.update(findUser.user_id, { vender_radius: updateRadiusDto.vender_radius });
+
+    const updateUser: User = await UserEntity.findOne({ where: { user_id: findUser.user_id } });//TODO change interface to user
+    updateUser.user_password = undefined;
+    updateUser.user_activation_code= undefined;
+    return updateUser;
+  }
+
+  //updateUser update user by id
+  public async verifyUser(verifyUserDto: VerifyUserDto): Promise<User> {
+    if (isEmpty(verifyUserDto)) throw new HttpException(400, "verifyUserDto is empty");
+
+    const user_found: User = await UserEntity.findOne({ where: { user_id: verifyUserDto.user_id, user_login: verifyUserDto.user_login} });
+    if (!user_found) throw new HttpException(409, "User doesn't exist");
+    if(user_found.user_activation_code != verifyUserDto.user_activation_code) throw new HttpException(409, "User activation code is not correct");
+
+    await UserEntity.update(user_found.user_id, { user_status: true });
+
+    const updateUser: User = await UserEntity.findOne({ where: { user_id: user_found.user_id } });//TODO change interface to user
+    updateUser.user_password = undefined;
+    updateUser.user_activation_code= undefined;
+    return updateUser;
   }
 }
 

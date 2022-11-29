0e0e03b92d073f6f00c7195d25cfdc77c3a6489b
azeez-ahmad <azeez.aziiz@gmail.com>
2022-11-29 16:59:16 -0500

    fix subscriotion route

--- meal-dash-business/src/routes/subscription.route.ts
+++ meal-dash-business/src/routes/subscription.route.ts
@@ -3,6 +3,7 @@ import SubscriptionController from '@controllers/subscription.controller';
 import { SubscriptionDto } from '@/dtos/subscriptions.dto';
 import { Routes } from '@interfaces/routes.interface';
 import validationMiddleware from '@middlewares/validation.middleware';
+import authMiddleware from '@/middlewares/auth.middleware';
 
 // SubscriptionRoute class to handle all the routes for subscriptions
 class SubscriptionRoute implements Routes {
@@ -16,7 +17,7 @@ class SubscriptionRoute implements Routes {
 
   private initializeRoutes() {
     console.log('--- routes ---- ');
-    this.router.post(`${this.path}`, validationMiddleware(SubscriptionDto, 'body'), this.subscriptionController.createSubscriptionPlan);
+    this.router.post(`${this.path}`, authMiddleware, validationMiddleware(SubscriptionDto, 'body'), this.subscriptionController.createSubscriptionPlan);
   }
 }
 

2d09ee6efcd820ebe3fefdce98a96615e6a02f7b
azeez-ahmad <azeez.aziiz@gmail.com>
2022-11-29 16:57:06 -0500

    subscription role check

--- meal-dash-business/src/controllers/subscription.controller.ts
+++ meal-dash-business/src/controllers/subscription.controller.ts
@@ -2,16 +2,23 @@ import { NextFunction, Request, Response } from 'express';
 import { SubscriptionDto } from '@/dtos/subscriptions.dto';
 import { Subscription } from '@interfaces/subscription.interface';
 import SubscriptionService from '@services/subscription.service';
+import { RequestWithUser } from '@/interfaces/auth.interface';
+import { USER_ROLES } from '@/utils/util';
+import { HttpException } from '@/exceptions/HttpException';
 
 class SubscriptionController {
   public subscriptionService = new SubscriptionService();
 
-  public createSubscriptionPlan = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  public createSubscriptionPlan = async (req: RequestWithUser, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const subscriptionData: SubscriptionDto = req.body;
-      const createSubscriptionData: Subscription = await this.subscriptionService.createSubscription(subscriptionData);
+      if (req.user_role && req.user_role === USER_ROLES.API_ACCESS) {
+        const subscriptionData: SubscriptionDto = req.body;
+        const createSubscriptionData: Subscription = await this.subscriptionService.createSubscription(subscriptionData);
 
-      res.status(201).json({ data: createSubscriptionData, message: 'created' });
+        res.status(201).json({ data: createSubscriptionData, message: 'created' });
+      } else {
+        throw new HttpException(403, 'Unauthorized access');
+      }
     } catch (error) {
       next(error);
     }

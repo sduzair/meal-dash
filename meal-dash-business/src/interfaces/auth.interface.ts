82914060f2aa6764e781d0c239d3e2f58a180782
azeez-ahmad <azeez.aziiz@gmail.com>
2022-11-29 16:58:16 -0500

    updated auth interface for role

--- meal-dash-business/src/interfaces/auth.interface.ts
+++ meal-dash-business/src/interfaces/auth.interface.ts
@@ -3,6 +3,7 @@ import { User } from '@interfaces/users.interface';
 
 export interface DataStoredInToken {
   user_id: number;
+  user_role: string;
 }
 
 export interface TokenData {
@@ -12,10 +13,12 @@ export interface TokenData {
 
 export interface RequestWithUser extends Request {
   user: User;
+  user_role: string;
 }
 
 export interface RequestWithUserAndFile extends Request {
   user: User;
+  user_role: string;
   files: any;
   fields: any;
 }
